b68a1df67deef7cf95bd364705b94bc4fe857a0d
William Ledo <williamledoc@gmail.com>
2022-03-14 19:44:11 -0300

    Project created

---
+++ backend/src/test/java/com/williamledo/wlmovie/WlmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamledo.wlmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
